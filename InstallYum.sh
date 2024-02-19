0e920dc3e9fae3f8493d373833ecc693ee02075c
Ashkan <AshkanWatson@gmail.com>
2024-02-19 12:07:53 +0330

    This Is For YUM Package Tool

---
+++ InstallYum.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Check if firewalld is installed
+if ! command -v firewall-cmd &> /dev/null
+then
+    # Install firewalld
+    sudo yum install firewalld -y
+fi
+
+# Enable and start firewalld
+sudo systemctl enable firewalld && sudo systemctl start firewalld
+
+# Allow specific ports for incoming traffic
+sudo firewall-cmd --permanent --add-port=22/tcp
+sudo firewall-cmd --permanent --add-port=22/udp
+sudo firewall-cmd --permanent --add-port=443/tcp
+sudo firewall-cmd --permanent --add-port=443/udp
+sudo firewall-cmd --permanent --add-port=2060/tcp
+sudo firewall-cmd --permanent --add-port=2060/udp
+sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=80/udp
+
+# Reload the firewall to apply changes
+sudo firewall-cmd --reload
+
+# Update package lists
+sudo yum update -y
+
+# Upgrade packages to the latest version
+sudo yum upgrade -y
+
+# Install the latest versions of all packages installed on the system
+sudo yum distro-sync -y
+
+# Upgrade to the latest version of the distribution
+sudo yum update -y
+
+# Install Certbot For SSL
+sudo yum install certbot python3-certbot-nginx -y
+
+sudo systemctl stop nginx
+
+read -p "Enter Your Domain Name (e.g., my-domain.com): " DOMAIN
+
+# Check if Certbot is installed and install if necessary
+if ! command -v certbot &> /dev/null
+then
+    sudo yum install certbot -y
+fi
+
+# Generate Let's Encrypt SSL certificate using Certbot
+sudo certbot certonly --standalone --preferred-challenges http -d $DOMAIN
+
+# Copy certificate files to /root
+sudo cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem /root
+sudo cp /etc/letsencrypt/live/$DOMAIN/privkey.pem /root
+
+# Change permissions of copied files
+chmod 600 /root/fullchain.pem
+chmod 600 /root/privkey.pem
+
+sudo systemctl start nginx
